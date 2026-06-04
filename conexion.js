107ef91f9b25843ade6cb68dc6c6838a3e4ca7a7
emanuelamaya200-source <emanuelamaya200@gmail.com>
2026-06-04 13:07:26 -0300

    configurar puerto 2

--- conexion.js
+++ conexion.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     dialectModule: pg,
     logging: false,
